f25e4ef9a8f452825e6826a00a814db3b752abc9
MarilynMaika <127025830+MarilynMaika@users.noreply.github.com>
2025-05-22 17:01:20 +0300

    Delete load_data.sql

--- load_data.sql
+++
@@ -1,5 +0,0 @@
-
-COPY dim_date FROM '/path/to/dim_date.csv' DELIMITER ',' CSV HEADER;
-COPY dim_product FROM '/path/to/dim_product.csv' DELIMITER ',' CSV HEADER;
-COPY dim_store FROM '/path/to/dim_store.csv' DELIMITER ',' CSV HEADER;
-COPY fact_sales FROM '/path/to/fact_sales.csv' DELIMITER ',' CSV HEADER;
